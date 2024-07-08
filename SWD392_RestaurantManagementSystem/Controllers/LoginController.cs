c6a8a375f61548e2d296c9f8c491c31a9868c21c
phamquy2602 <115681413+phamquy2602@users.noreply.github.com>
2024-07-08 11:01:19 +0700

    RMS Login

---
+++ SWD392_RestaurantManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using SWD392_RestaurantManagementSystem.Models;
+using SWD392_RestaurantManagementSystem.Services;
+
+namespace SWD392_RestaurantManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly RestaurantManagementContext _context;
+
+        public LoginController(RestaurantManagementContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Username == username && u.PasswordHash == password);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = "Invalid username or password.";
+                return View();
+            }
+
+            // Lưu vai trò vào session
+            HttpContext.Session.SetInt32("RoleId", user.RoleId);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
